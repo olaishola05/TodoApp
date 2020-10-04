b3c6013c3a62fedd6a447bd60d24d38babc9a9b6
Ola Ishola <olaishola@hotmail.co.uk>
2020-10-04 21:04:20 +0100

    commit no 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Todo from "./components/Todo";
+import todoData from "./components/TodoData";
+import ButtonCommponent from "./components/ButtonComponent";
+import AddTodo from "./components/AddTodo";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: todoData,
+        };
+
+        // this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (id) => {
+        this.setState((prevState) => {
+            const updateData = prevState.data.map((todo) => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            });
+
+            return {
+                data: updateData,
+            };
+        });
+    };
+
+    //  Delete Todo from list
+    delTodo = (id) => {
+        this.setState((prevState) => {
+            const updateState = prevState.data.filter((todo) => todo.id !== id);
+            return {
+                data: updateState,
+            };
+        });
+    };
+
+    addTodo = (text) => {
+        const newTodo = {
+            id: uuidv4,
+            text: text,
+            completed: false,
+        };
+        this.setState((prevState) => {
+            return {
+                data: [...prevState.data, newTodo],
+            };
+        });
+    };
+    render() {
+        const TodoItems = this.state.data.map((todo) => <Todo key={todo.id} todo={todo} handleChange={this.handleChange} delTodo={this.delTodo} />);
+        return (
+            <div className="app">
+                <AddTodo addTodo={this.addTodo} />
+                <ButtonCommponent />
+                {TodoItems}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,34 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
-function AddTodo() {
-    return (
-        <div style={addTodo}>
-            <input style={inputField} placeholder="add todo" />
-            <button style={btnStyle}>+</button>
-        </div>
-    );
+export class AddTodo extends Component {
+    constructor() {
+        super();
+        this.state = {
+            text: "",
+        };
+    }
+    onChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value,
+        });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.text);
+        this.setState({ text: "" });
+    };
+    render() {
+        return (
+            <div className="todo">
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="text" value={this.state.text} placeholder="Add new todo" onChange={this.onChange} />
+                    <button>Add Todo</button>
+                </form>
+
+                <p>{this.state.text}</p>
+            </div>
+        );
+    }
 }
 
-const addTodo = {
-    display: "flex",
-    flexDirection: "row",
-    height: "30px",
-    width: "50%",
-    margin: "0 auto",
-    marginTop: "20px",
-};
-
-const inputField = {
-    width: "100%",
-    height: "25px",
-    marginBottom: "10px",
-};
-
-const btnStyle = {
-    width: "70px",
-    marginLeft: "10px",
-    fontSize: "1.5rem",
-    background: "black",
-    color: "white",
-};
 export default AddTodo;
